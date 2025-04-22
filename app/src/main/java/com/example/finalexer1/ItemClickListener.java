cbb96f2d7abc9a50e26672f877b8854f2d68be48
JodieLyn04 <gutierrez_jodielyn@plpasig.edu.ph>
2025-04-22 11:24:39 +0800

    error

--- app/src/main/java/com/example/finalexer1/ItemClickListener.java
+++ app/src/main/java/com/example/finalexer1/ItemClickListener.java
@@ -3,6 +3,6 @@ package com.example.finalexer1;
 import android.view.View;
 
 public interface ItemClickListener {
-    public void onClick(View view, int position);
+     void onClick(View view, int position);
 
 }
